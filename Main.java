9e3d84736d18b6f72426badf0c8e8424ea1a543b
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-06-10 18:22:08 +0000

    added notes

--- Main.java
+++ Main.java
@@ -3,3 +3,8 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+/*
+Static -> We want to run this function without creating an object of class Main
+Void -> It defines the return type of the function (integer + integer is integer)
+String[] args -> Arguments or command line arguments
+*/
