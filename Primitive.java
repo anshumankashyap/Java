15f37048b58d4b524a4cd2378eb6648bc0313f47
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-06-11 08:45:23 +0000

    Primitive Data Types

---
+++ Primitive.java
@@ -0,0 +1,12 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int roll = 64;
+        char letter = 'a';
+        double largeDecimal = 4554544646.559668;
+        float Marks = 45.90;
+        long largeInteger = 123456789446468L;
+        boolean check = False;
+    }
+}
+/* Primitive Data type -> those data type which can not be break further such as the "letter R or any digit" is known as
+primitive data type */
