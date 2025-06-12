b717c22b26b701b98abe446d098caf9453865ae8
Ansuman Panda <124714817+anshumankashyap@users.noreply.github.com>
2025-06-12 17:27:04 +0000

    Java Inputs

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+        System.out.print("Please give some inputs ");
+        int rollNo = inputs.nextInt();
+        System.out.println("Your roll no is " + rollNo);
+    }
+}
